b1e024f668b5faf075810f34016428ec90c1c242
toppy368 <toppy36849@gmail.com>
2018-03-29 00:42:00 +0800

    定義變數

--- recopy_exe.py
+++ recopy_exe.py
@@ -1,3 +1,12 @@
 import os
+#Hello world 指令
 printf = "Notepad.exe"
+
+#定義參數
+Robocopy = "robocopy"
+#來源路徑，使用者請修改它
+Source = "D:\Source folder path"
+#目的地路徑
+Destination = "D:\Destination folder path"
+
 os.system(printf)
