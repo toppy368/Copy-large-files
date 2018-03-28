5bf9781977ea9075f8f1b759781138835c871f67
toppy368 <toppy36849@gmail.com>
2018-03-29 00:50:21 +0800

    將定義分類

--- recopy_exe.py
+++ recopy_exe.py
@@ -2,15 +2,21 @@ import os
 #Hello world 指令
 printf = "Notepad.exe"
 
-#定義參數
+#定義指令與主要路徑
 Robocopy = "robocopy"
 #來源路徑，使用者請修改它
 Source = "D:\Source folder path"
 #目的地路徑
 Destination = "D:\Destination folder path"
 
+#定義參數
 #鏡像複製
 #注意：此指令會造成目的地資料遺失
 Mirrors_Copy = "/mir"
+
+#遞迴複製(含空資料夾)
+#說明：此指令也會複製子資料夾
+recursive_Mode = "/e"
+
 
 os.system(printf)
