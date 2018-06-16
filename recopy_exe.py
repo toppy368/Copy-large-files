e94efe84ea5e6db7cc5b448d57f31193bce27046
Unknown <toppy36849@gmail.com>
2018-06-16 16:09:13 +0800

    Auto stash before merge of "master" and "origin/master"

--- recopy_exe.py
+++ recopy_exe.py
@@ -3,7 +3,7 @@ import os
 printf = "Notepad.exe"
 
 #定義指令與主要路徑
-Robocopy = 'robocopy'
+
 #來源路徑，使用者請修改它
 Source = 'D:\TEST\A' + ' '
 #目的地路徑
@@ -11,14 +11,14 @@ Destination = 'D:\TEST\B' + ' '
 
 #定義參數
 #鏡像複製
-#注意：此指令會先清除目的地資料夾的資料 
-Mirrors_Copy = '/mir'
+#注意：此指令會先清除目的地資料夾的資料
+#Mirrors_Copy = '/mir'
 
 #遞迴複製(含空資料夾)
 #說明：此指令也會複製子資料夾
-Recursive_Mode = '/e'
+#Recursive_Mode = '/e'
 
 #多線城複製設定(此指令適用於Win7以上)
-Multi_threaded_set = '/mt:100'
+#Multi_threaded_set = '/mt:100'
 
 os.system('robocopy '+  Source + Destination + '/mir /mt:100')
